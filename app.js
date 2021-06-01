b2ef1b4ad748a1906393377066f13af1ea7ca531
DinhNPVy <4501104282@student.hcmue.edu.vn>
2021-06-01 19:52:01 -0400

    upload

---
+++ app.js
@@ -0,0 +1,31 @@
+let value = +prompt("Nhập số bất kì từ  1 -> 7  để biết thứ: ")
+
+switch (value) {
+    case 1:
+        alert("Chủ Nhật")
+        break;
+    case 2:
+        alert("Thứ hai")
+        break;
+    case 3:
+        alert("Thứ ba")
+        break;
+    case 4:
+        alert("Thứ tư")
+        break;
+    case 5:
+        alert("Thứ năm")
+        break;
+    case 6:
+        alert("Thứ sáu")
+        break;
+    case 7:
+        alert("Thứ bảy")
+        break;
+    case 8:
+        alert("Chủ Nhật")
+        break;
+
+    default:
+        break;
+}
